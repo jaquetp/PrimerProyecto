2b311915a4d3577bd85570a404c29dd878b4d850
jaquetp <jaq_tp@outlook.com>
2016-08-23 21:58:33 -0500

    Update CompartirCodigo.c
    Compartir con Gaby

--- CompartirCodigo.c
+++ CompartirCodigo.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
- printf(“Este es mi segundo codigo”);
- printf(“Programar es facil”);
- printf(“Voy a compartir con mi compañero el codigo”);
+ printf("Este es mi segundo codigo\n");
+ printf("Programar es facil\n");
+ printf("Voy a compartir con mi compañero el codigo\n");
+ printf("Gaby\n");
  return 0;
 }
